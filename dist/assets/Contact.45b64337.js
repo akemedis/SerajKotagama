1e4ef9c52872cd42ef72f07e9e8a34dd49e4976b
akemedis <41940887+akemedis@users.noreply.github.com>
2023-01-04 21:35:03 +1100

    2 files modified, 6 added

---
+++ dist/assets/Contact.45b64337.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as t}from"./index.8e8f528a.js";const n={};function o(r,a){return e(),t("h2",null,"Contact")}var s=c(n,[["render",o]]);export{s as default};
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import Vuetify from 'vuetify'
 
-const app = createApp(App)
+const app = createApp(App, {
+  theme: {
+    primary: '#3f51b5',
+    secondary: '#FFCDD2',
+    accent: '#ff4081',
+    danger: '#F44336',
+  },
+})
+app.use(Vuetify)
 app.use(router)
 app.mount('#app')
