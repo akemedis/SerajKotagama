6a4a7b46e822b8c6401048d4375ef389760cd31c
akemedis <41940887+akemedis@users.noreply.github.com>
2023-01-04 23:32:29 +1100

    2 files modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,37 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
-    fontFamily: {
-      sans: [
-        'Inter',
-        'Avenir',
-        'Roboto',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Ubuntu',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
+    theme: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        blue: '#1fb6ff',
+        purple: '#7e5bef',
+        pink: '#ff49db',
+        orange: '#ff7849',
+        green: '#13ce66',
+        yellow: '#ffc82c',
+        'gray-dark': '#273444',
+        gray: '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      extend: {
+        spacing: {
+          128: '32rem',
+          144: '36rem',
+        },
+        borderRadius: {
+          '4xl': '2rem',
+        },
+      },
     },
   },
   plugins: [],
